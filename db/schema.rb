79f6d70793eff6644eb6360b5d74020e52f50813
crespire <li.simmon@gmail.com>
2022-02-16 15:20:38 -0500

    Update schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_194806) do
+ActiveRecord::Schema.define(version: 2022_02_16_201522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,4 +33,6 @@ ActiveRecord::Schema.define(version: 2022_02_16_194806) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "flights", "airports", column: "destination_airport_id"
+  add_foreign_key "flights", "airports", column: "origin_airport_id"
 end
