12454c43fef7399b6b9bdb042afe55fa8cda0181
crespire <li.simmon@gmail.com>
2022-02-16 20:34:16 -0500

    Update routes to set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'flights#index'
+  get '/airports', to: redirect('/flights')
+
   resources :flights
-  resources :airports
+  # resources :airports
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
