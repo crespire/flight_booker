de82933de0ace3e6d76d3846e50727248b6b632d
crespire <li.simmon@gmail.com>
2022-02-17 11:12:12 -0500

    Add search partial to flights views

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
-  belongs_to :destination, class_name: 'Airport', foreign_key: :destination_airport_id
-  belongs_to :origin, class_name: 'Airport', foreign_key: :origin_airport_id
+  belongs_to :destination, class_name: 'Airport',
+                           foreign_key: 'destination_airport_id'
+
+  belongs_to :origin, class_name: 'Airport',
+                      foreign_key: 'origin_airport_id'
 end
