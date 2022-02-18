5299bed8600343ff64923335b048b525121312dc
crespire <li.simmon@gmail.com>
2022-02-18 12:57:22 -0500

    Update Passenger to also belong to flight

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,5 @@ class Passenger < ApplicationRecord
   validates :email, format: { with: /\A(\S+)@(.+)\.(\S+)\z/, message: 'format invalid' }, presence: true
 
   belongs_to :booking
+  belongs_to :flight, through: :booking
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,17 @@ airline_callsigns = %w[AC AS AA DL FX NW SW WS]
 
 25.times do
   (1..8).to_a.each do |i|
-    Flight.create(name: "%s-%03d" % [airline_callsigns.sample, rand(1..999)], depart_time: DateTime.now + rand(100..600), depart_date: Date.today + rand(2..60), flight_duration: rand(43..458.0), origin_airport_id: i, destination_airport_id: rand(1..8))
+    destinations = (1..8).to_a
+    destinations.delete(i)
+    Flight.create(name: "%s-%03d" % [airline_callsigns.sample, rand(1..999)], depart_time: DateTime.now + rand(100..600), depart_date: Date.today + rand(2..60), flight_duration: rand(43..458.0), origin_airport_id: i, destination_airport_id: destinations.sample)
   end
 end
 
 puts "Created 25 flights originating from each airport, #{25 * 8} total."
+
+Flight.all.each do |flight|
+  booking = flight.bookings.create
+  2.times { booking.passengers.create(flight_id: flight.id, name: 'Jim', email: 'test@test.com') }
+end
+
+puts "Created a booking with 2 passengers for all flights."
